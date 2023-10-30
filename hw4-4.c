cf0cfd842e9d2865021c735f64ffc700069a7a3b
112ab0058 <146065141+112ab0058@users.noreply.github.com>
2023-10-30 22:21:07 +0800

    Update hw4-4.c

--- hw4-4.c
+++ hw4-4.c
@@ -5,15 +5,12 @@ int main() {
     scanf("%d", &n);
 
     int matrix[n][n];
-
-    // 读入好感度矩阵
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             scanf("%d", &matrix[i][j]);
         }
     }
 
-    // 配对算法
     for (int i = 0; i < n; i++) {
         int bestMatch = -1;
         int bestScore = -1;
