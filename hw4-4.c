7ba446199e1762cb7ad526af3edc99b9552f9946
112ab0058 <146065141+112ab0058@users.noreply.github.com>
2023-10-30 22:20:49 +0800

    Update hw4-4.c

--- hw4-4.c
+++ hw4-4.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
 
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int matrix[n][n];
+
+    // 读入好感度矩阵
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    // 配对算法
+    for (int i = 0; i < n; i++) {
+        int bestMatch = -1;
+        int bestScore = -1;
+
+        for (int j = 0; j < n; j++) {
+            if (matrix[i][j] > bestScore) {
+                bestScore = matrix[i][j];
+                bestMatch = j;
+            }
+        }
+
+        printf("boy %d pair with girl %d.\n", i + 1, bestMatch + 1);
+    }
+
+    return 0;
+}
